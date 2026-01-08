d117099643bbdc873fda524c959a265124d6f5da
Mostafa <mostafa@uconn.edu>
2026-01-07 19:12:13 -0500

    Improved type checking and change to a class
    Added class and enclosed the column lists
    Added type checking

--- src/elastin_fiber_utils/dataloader.py
+++ src/elastin_fiber_utils/dataloader.py
@@ -3,42 +3,51 @@ import pandas as pd
 from . import lazy_loader
 from . import pulling_data
 
-COULMNS = ['Step', 'Pxx', 'Pyy', 'Lx', 'Ly', 'Xlo', 'Xhi', 'Ylo', 'Yhi', 
+class prepareData:
+    def __init__(self):
+        self._columns = ['Step', 'Pxx', 'Pyy', 'Lx', 'Ly', 'Xlo', 'Xhi', 'Ylo', 'Yhi', 
             'PotEng', 'KinEng', 'Temp', 'v_max_bond_length', 'f_111[1]',
             'f_111[2]'] 
+        
+    def update_columns(self, columns):
+        self._columns = columns
 
-def extract_log_data(log_file: str, data_name: str,):
-    """
-    Extracts the data from the log file
-    data name: saved npz file
-    log file: lammps log file path
-    """
-    data = lazy_loader.lazy_loader(data_name, log_file)
-    data.read(log_file)
-    data.save()
+    def get_columns(self):
+        return self._columns
 
-def load_log_data(data_name: str):
-    return np.load(data_name, allow_pickle=True)
+    def load_to_memory(self, isLog: bool, **kwargs) -> pulling_data.pulling_data:
+        if isLog:
+            self.extract_log_data(kwargs['log_file'], kwargs['data_name'])
+        raw_data = self.load_log_data(kwargs['data_name'])
+        raw_data_prepared = self.prepare_data(raw_data)
+        pulling_numbers = pulling_data.pulling_data(raw_data_prepared)
+        pulling_numbers.pre_process(kwargs['pulling_direction'])
+        return pulling_numbers
 
-def prepare_data(data: np.ndarray) -> pd.DataFrame:
-    captured_states = len(data)
-    states_data = np.zeros((int(data[f'arr_{captured_states-1}'][0]), 15))
+    def extract_log_data(self, log_file: str, data_name: str,):
+        """
+        Extracts the data from the log file
+        data name: saved npz file
+        log file: lammps log file path
+        """
+        data = lazy_loader.lazy_loader(data_name, log_file)
+        data.read(log_file)
+        data.save()
 
-    i = 0
-    for value in data.values():
-        if value.shape[0] == 1:
-            states_data[i, :] = value
-            i += 1
+    def load_log_data(self,data_name: str):
+        return np.load(data_name, allow_pickle=True)
 
-    states_data = pd.DataFrame(states_data, columns=COULMNS)
+    def prepare_data(self, data: np.lib.npyio.NpzFile) -> pd.DataFrame:
+        captured_states = len(data)
+        states_data = np.zeros((int(data[f'arr_{captured_states-1}'][0]), 15))
 
-    return states_data
+        i = 0
+        for value in data.values():
+            if value.shape[0] == 1:
+                states_data[i, :] = value
+                i += 1
 
-def disk_to_memory_workflow(npz_lib_path: str, pulling_direction: str) -> pulling_data.pulling_data:
+        states_data = pd.DataFrame(states_data, columns=self._columns)
 
-    raw_data = load_log_data(npz_lib_path)
-    raw_data_prepared = prepare_data(raw_data)
-    pulling_numbers = pulling_data.pulling_data(raw_data_prepared)
-    pulling_numbers.pre_process(pulling_direction)
+        return states_data
 
-    return pulling_numbers
