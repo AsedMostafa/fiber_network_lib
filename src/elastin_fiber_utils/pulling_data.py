679c1863afa22a255c60d7475203d9f962e0cd65
Mostafa <mostafa@uconn.edu>
2026-02-15 11:37:27 -0500

    Add hyper elasticy fit with better data access
    Stress and strain now can be accessed by using "dot" method.
    Columns that are exists can be accessed with avalable data.
    Yeoh incompressible included

--- src/elastin_fiber_utils/pulling_data.py
+++ src/elastin_fiber_utils/pulling_data.py
@@ -1,22 +1,45 @@
 import numpy as np
 import pandas as pd
+from utils import yeoh_incompressible
+from scipy.optimize import curve_fit
 
 class pulling_data:
     def __init__(self, data):
-        self.data = data
+        self.data: pd.DataFrame = data
         self.break_strain_index = None    
         self.elasticity_modulus = None
         self.toughness = None
         self.max_stress = None
         self.failure_strain = None
+        self.params:dict = {}
+        self.models = {'yeoh_incompressible': yeoh_incompressible}
 
     def make_stress_strain(self, p_dir: str):
         pulling_direction_map = {'x': ['Lx', 'Pxx'], 'y': ['Ly', 'Pyy']}
         length = pulling_direction_map[p_dir][0]
         pressure = pulling_direction_map[p_dir][1]
         self.data['strain'] = (self.data[length] - self.data[length][0]) / self.data[length][0] 
-        self.data['stress'] = -1 * self.data[pressure] * 101325 * 1e-9
+        self.data['stress'] = -1 * self.data[pressure] * 0.5 * 101325 * 1e-9
         self.data.rename(columns={'f_111[2]': 'broken_bonds'}, inplace=True)
+
+    @property
+    def stress(self):
+        return self.data['stress'].to_numpy()
+    
+    @property
+    def strain(self):
+        return self.data['strain'].to_numpy()
+    
+    @property
+    def available_data(self):
+        return self.data.columns
+
+    def fit_behavior(self, model: str):
+        _model = self.models[model]
+        params, _ = curve_fit(_model, self.strain[:1300]+1, 
+                       self.stress[:1300])
+        self.params[model] = params  
+        
 
     def pre_process(self, pulling_direction: str):
         self.make_stress_strain(pulling_direction)
@@ -25,6 +48,7 @@ class pulling_data:
         self.toughness = self.get_toughness(self.data)
         self.max_stress = self.data['stress'].max()
         self.failure_strain = self.data['strain'][self.data['stress'].idxmax()]
+        self.fit_behavior('yeoh_incompressible')
 
     def get_toughness(self, df: pd.DataFrame):
         max_stress = df['stress'].max()
